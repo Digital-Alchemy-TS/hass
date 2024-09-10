026ea25f6bdc580703e3694d6f81d67e9b29287c
James Hollowell <jamesthollowell@gmail.com>
2024-09-09 20:33:34 -0400

    feat: fix lint errors and format JSDoc

--- src/helpers/fetch/weather-forecasts.ts
+++ src/helpers/fetch/weather-forecasts.ts
@@ -1,4 +1,3 @@
-
 // Comes from https://www.home-assistant.io/integrations/weather/#action-weatherget_forecasts
 
 export type WeatherCondition =
@@ -20,16 +19,16 @@ export type WeatherCondition =
 
 export interface WeatherGetForecasts {
   /**
-   * Time of the forcasted conditions.
+   * Time of the forecasted conditions.
    * Format is YYYY-MM-DDTHH:mm:ss+zz:zz
    */
   datetime: string;
   /**
-   * Only set for <code>twice_daily</code> forecasts
+   * Only set for `twice_daily` forecasts
    */
   is_daytime: boolean;
   /**
-   * The apparent (feels-like) temperature in the unit indicated by the <code>temperature_unite</code> state attribute.
+   * The apparent (feels-like) temperature in the unit indicated by the `temperature_unite` state attribute.
    */
   apparent_temperature: number;
   /**
@@ -41,31 +40,31 @@ export interface WeatherGetForecasts {
    */
   condition: WeatherCondition;
   /**
-   * The dew point temperature in the unit indicated by the <code>temperature_unite</code> state attribute.
+   * The dew point temperature in the unit indicated by the `temperature_unite` state attribute.
    */
   dew_point: number;
-/**
- * The relative humidity in %.
- */
+  /**
+   * The relative humidity in %.
+   */
   humidity: number;
   /**
    * The probability of precipitation in %;
    */
   precipitation_probability: number;
   /**
-   * The precipitation amount in the unit indicated by the <code>precipitation_unit</code> state attribute.
+   * The precipitation amount in the unit indicated by the `precipitation_unit` state attribute.
    */
   precipitation: number;
   /**
-   * The air pressure in the unit indicated by the <code>pressure_unit</code> state attribute.
+   * The air pressure in the unit indicated by the `pressure_unit` state attribute.
    */
   pressure: number;
   /**
-   * The temperature in the unit indicated by the <code>temperature_unite</code> state attribute. If <code>templow</code> is also provided this is the higher temperature.
+   * The temperature in the unit indicated by the `temperature_unite` state attribute. If `templow` is also provided this is the higher temperature.
    */
   temperature: number;
   /**
-   * The lower temperature in the unit indicated by the <code>temperature_unite</code> state attribute.
+   * The lower temperature in the unit indicated by the `temperature_unite` state attribute.
    */
   templow: number;
   /**
@@ -77,11 +76,11 @@ export interface WeatherGetForecasts {
    */
   wind_bearing: number | string;
   /**
-   * The wind gust speed in the unit indicated by the <code>wind_speed_unit</code> state attribute.
+   * The wind gust speed in the unit indicated by the `wind_speed_unit` state attribute.
    */
   wind_gust_speed: number;
   /**
-   * The wind speed in the unit indicated by the <code>wind_speed_unit</code> state attribute.
+   * The wind speed in the unit indicated by the `wind_speed_unit` state attribute.
    */
   wind_speed: number;
 }
